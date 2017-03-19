edfd840e54224658020bb727be6849abfedd1776
fahadali <fahadali@192.168.0.100>
2017-03-19 19:32:46 +0100

    Card & Deck created

---
+++ src/entities/ChanceCard.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class ChanceCard {
+	
+	private int cardValue ;
+	private  String cardText;
+	
+	
+	public ChanceCard(int cardValue, String cardText) {
+	
+		this.cardValue = cardValue;
+		this.cardText = cardText;
+	}
+
+
+	public int getCardValue() {
+		return cardValue;
+	}
+
+
+	public void setCardValue(int cardValue) {
+		this.cardValue = cardValue;
+	}
+
+
+	public String getCardText() {
+		return cardText;
+	}
+
+
+	public void setCardText(String cardText) {
+		this.cardText = cardText;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ChanceCard [cardText =" + cardText + "]";
+	}
+
+
+	
+	
+	
+	
+	
+	
+
+}
