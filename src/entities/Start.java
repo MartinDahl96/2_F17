c82372068d5b86a5f446e2b2c06bcf360e0c0aa8
fahadali <fahadali@192.168.10.85>
2017-03-23 14:58:28 +0100

    Start changes

--- src/entities/Start.java
+++ src/entities/Start.java
@@ -1,33 +1,45 @@
 package entities;
 
 public class Start implements Field {
+	
+	String fieldName;
+	
+	
+	public Start(String fieldName, String fieldInfo, int startBonus){
+		this.fieldName = fieldName;
+		
+	}
+
 
 	@Override
-	public void setFieldName(String text) {
-		// TODO Auto-generated method stub
+	public void setFieldName(String fieldName) {
 		
 	}
 
 	@Override
 	public String getFieldName() {
-		// TODO Auto-generated method stub
-		return null;
+		return fieldName;
 	}
 
 	@Override
-	public void setFieldInfo(String text) {
-		// TODO Auto-generated method stub
+	public void setFieldInfo(String fieldInfo) {
 		
 	}
 
 	@Override
 	public String getFieldInfo() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	
-	public void landOnField(){
+	public void landOnField(Player player){
 		
+		
+		
+	}
+	
+	@Override
+	public String toString() {
+		return fieldName;
 	}
 
 }
