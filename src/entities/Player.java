baa0a3242fc26be253acb1f8d96481ac315c2bba
mortentang <mortentc@hotmail.com>
2017-03-28 16:25:46 +0200

    Comments
    More comments and method body for Brewery rent

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -7,6 +7,7 @@ public class Player {
 	private int ID;
 	private int jailRounds;
 	private int ownedFerries;
+	private int ownedBreweries;
 	private int currentPosition;
 	public boolean immunity;
 	private int fortune;
@@ -27,86 +28,100 @@ public class Player {
 
 	}
 
-	public int getJailToken() {
-		return jailToken;
-	}
+	// used to get the amount of jailTokens held by a player.
+		public int getJailToken() {
+			return jailToken;
+		}
 
-	public void setJailToken(int jailToken) {
-		this.jailToken = jailToken;
-	}
+		//used to set the amount of jailTokens held by a player.
+		public void setJailToken(int jailToken) {
+			this.jailToken = jailToken;
+		}
+		
+		//used to see if a player is immune or not.
+		public boolean isImmunity() {
+			return immunity;
+		}
 
-	public boolean isImmunity() {
-		return immunity;
-	}
+		//used to set a player's immunity.
+		public void setImmunity(boolean immunity) {
+			this.immunity = immunity;
+		}
 
-	public void setImmunity(boolean immunity) {
-		this.immunity = immunity;
-	}
+		// Returns the name of the player.
+		public String getplayerName() {
+			return playerName;
+		}
 
-	// Returns the name of the player.
-	public String getplayerName() {
-		return playerName;
-	}
+		// Sets the name of a player.
+		public void setplayerName(String name) {
+			playerName = name;
+		}
 
-	// Sets the name of a player.
-	public void setplayerName(String name) {
-		playerName = name;
-	}
+		// Returns the player's balance.
+		public int getFortune() {
+			return this.fortune;
+		}
 
-	// Returns the player's balance.
-	public int getFortune() {
-		return this.fortune;
-	}
+		public void setFortune(int add) {
+			this.fortune += add;
+		}
 
-	public void setFortune(int add) {
-		this.fortune += add;
-	}
+		// gets the player's ID.
+		public int getPlayerNumber() {
+			return ID;
+		}
 
-	// gets the player's ID.
-	public int getPlayerNumber() {
-		return ID;
-	}
+		// sets the player's ID.
+		public void setPlayerNumber(int playerNumber) {
+			ID = playerNumber;
+		}
 
-	// sets the player's ID.
-	public void setPlayerNumber(int playerNumber) {
-		ID = playerNumber;
-	}
+		// gets the amount of rounds left of a player's jail-time.
+		public int getJailRounds() {
+			return jailRounds;
+		}
 
-	// gets the amount of rounds left of a player's jail-time.
-	public int getJailRounds() {
-		return jailRounds;
-	}
+		// sets the amount of rounds left of a player's jail-time.
+		public void setJailRounds(int jailRounds) {
+			this.jailRounds = jailRounds;
+		}
 
-	// sets the amount of rounds left of a player's jail-time.
-	public void setJailRounds(int jailRounds) {
-		this.jailRounds = jailRounds;
-	}
+		// gets the player's current position.
+		public int getCurrentPosition() {
+			return currentPosition;
+		}
 
-	// gets the player's current position.
-	public int getCurrentPosition() {
-		return currentPosition;
-	}
-
-	// sets the player's current position.
-	public void setCurrentPosition(int addToPosition) {
-		currentPosition = addToPosition + currentPosition;
-		if (currentPosition > 40) {
-			currentPosition -= 40; 
-			if (currentPosition > 0) {
-				setFortune(4000); // startBonus //genovervej skal denne være i playerklassen?
+		// sets the player's current position. ny adding an int to his current position.
+		public void setCurrentPosition(int addToPosition) {
+			currentPosition = addToPosition + currentPosition;
+			if (currentPosition > 40) {
+				currentPosition -= 40; 
+				if (currentPosition > 0) {
+					setFortune(4000); // startBonus //genovervej skal denne vÃ¦re i playerklassen?
+				}
 			}
 		}
+		
+		//used to set a player to a specific field.
+		public void changePosition (int newPosition){
+			this.currentPosition = newPosition;
+		}
+
+		//used to set the amount of ferries owned by a player.
+		public void setOwnedFerries(int ferriesOwned) {
+			ownedFerries = ownedFerries + ferriesOwned;
+		}
+		
+		//used to get the amount of ferries owned by a player.
+		public int getOwnedFerries() {
+			return ownedFerries;
 	}
-	
-	public void changePosition (int newPosition){
-		this.currentPosition = newPosition;
+	public void setOwnedBreweries(int breweriesOwned) {
+		ownedBreweries = ownedBreweries + breweriesOwned;
 	}
 
-	public void setOwnedFerries(int ferriesOwned) {
-		ownedFerries = ownedFerries + ferriesOwned;
-	}
-
-	public int getOwnedFerries() {
-		return ownedFerries;
+	public int getOwnedBreweries() {
+		return ownedBreweries;
 	}
 }
