f5d79b3617073302a8ec887bea9dad583a5f30f2
fahadali <fahadali@192.168.0.103>
2017-04-11 14:46:30 +0200

    MUI changes

--- src/controllers/MUI.java
+++ src/controllers/MUI.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import java.awt.Color;
+
 import desktop_resources.GUI;
+import entities.Player;
 
 /**
  * 
@@ -20,7 +23,55 @@ public class MUI {
 		GUI.showMessage(msg);
 	}
 
-	public static void setOwner(int position, String playerName){
+	public static void setOwner(int position, String playerName) {
 		GUI.setOwner(position, playerName);
 	}
+
+	public static String setSixButtons(String msg, String B1, String B2, String B3, String B4, String B5) {
+		return GUI.getUserButtonPressed(msg, B1, B2, B3, B4, B5);
+
+	}
+
+	public static String getUserString(String msg) {
+		return GUI.getUserString(msg);
+	}
+
+	public static Color carColor(int i) {
+		switch (i) {
+		case 1:
+			return Color.RED;
+		case 2:
+			return Color.GREEN;
+		case 3:
+			return Color.BLUE;
+		case 4:
+			return Color.BLACK;
+		case 5:
+			return Color.PINK;
+		default:
+			return Color.WHITE;
+		}
+	}
+
+	public static String nameValidation(String msg) {
+
+		String s;
+
+		do {
+			s = GUI.getUserString(msg); 
+			
+		} while (!s.matches("([a-zA-Z0-9]){2,10}") || s.matches("[0-9]+"));
+
+		// The first RegEx above matches any String containing Latin
+		// uppercase/lowercase letters, and that it is between 2-10 characters.
+		// The second RegEx ensures that the String doesn't consist of only
+		// numbers
+
+		// ("(.){2,10}")
+
+		return s;
+	}
+	
+	
+	
 }
