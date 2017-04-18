6326800e43bb640ba25ef5083f66aff69c247566
FAS16 <s160344@student.dtu.dk>
2017-04-18 13:03:54 +0200

    Add files via upload

---
+++ src/Nentities/ChanceDeck.java
@@ -0,0 +1,72 @@
+package entities;
+
+import java.util.*;
+
+import mainControllers.MUI;
+
+import java.io.FileNotFoundException;
+import java.io.File;
+
+public class ChanceDeck {
+
+	/**
+	 * Creates a stack of ChanceCards called deck.
+	 */
+	private Stack<ChanceCard> deck;
+
+	/**
+	 * Constructor used to create a stack pf ChanceCards.
+	 */
+	public ChanceDeck() {
+		this.deck = new Stack<ChanceCard>();
+		createDeck();
+		//shuffleDeck();
+	}
+
+	/**
+	 * Method used for creating a deck of ChanceCards.
+	 * Also throws an exception if the file "chanceCards" cannot be found.
+	 */
+	public void createDeck() {
+
+		try {
+			Scanner s = new Scanner(new File("txtfiles/chanceCards.txt"));
+
+			int tempID = 0;
+
+			while (s.hasNext()) {
+
+				String tempText = s.nextLine();
+				int tempValue = Integer.parseInt(s.nextLine());
+				deck.push(new ChanceCard(++tempID, tempValue, tempText));
+
+			}
+			s.close();
+		}
+
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Shuffles the created deck in order to randomize it.
+	 */
+	public void shuffleDeck() {
+		Collections.shuffle(this.deck);
+
+	}
+
+	/**
+	 * Used to display the deck.
+	 * @return deck.
+	 */
+	public Stack<ChanceCard> getDeck() {
+		return deck;
+	}
+	
+
+	
+
+}
---
+++ src/Nentities/Player.java
@@ -0,0 +1,146 @@
+package entities;
+
+public class Player {
+	// Attributes
+	private String playerName;
+	private Account account;
+	private int playerID;
+	private int jailRounds;
+	private int ownedFerries;
+	private int ownedBreweries;
+	private int currentPosition;
+	public boolean immunity;
+	private int fortune;
+	private int totalAssets;
+	private int jailToken;
+	private int prevPosition;
+
+	// Constructor for a player-object.
+	public Player(int playerID , String playerName) {
+		this.playerName = playerName;
+		this.playerID = playerID;
+		this.account = new Account(30000);
+		this.immunity = false;	
+		this.totalAssets = this.account.getBalance();
+		this.jailToken = 0;
+		this.prevPosition = 0;
+		fortune = this.account.getBalance();
+
+
+	}
+
+	// used to get the amount of jailTokens held by a player.
+		public int getJailToken() {
+			return jailToken;
+		}
+
+		//used to set the amount of jailTokens held by a player.
+		public void setJailToken(int add) {
+			jailToken += add;
+		}
+		
+		public int getTotalAssets(){
+			return this.totalAssets;
+		}
+		
+		public void setTotalAssets(int add){
+			totalAssets += add;
+		}
+	
+		
+		//used to see if a player is immune or not.
+		public boolean getImmunity() {
+			return immunity;
+		}
+
+		//used to set a player's immunity.
+		public void setImmunity(boolean immunity) {
+			this.immunity = immunity;
+		}
+
+		// Returns the name of the player.
+		public String getplayerName() {
+			return playerName;
+		}
+
+		// Sets the name of a player.
+		public void setplayerName(String name) {
+			playerName = name;
+		}
+
+		// Returns the player's balance.
+		public int getFortune() {
+			return this.fortune;
+		}
+
+		public void setFortune(int add) {
+			this.fortune += add;
+			this.totalAssets = fortune;
+		}
+
+		// gets the player's ID.
+		public int getPlayerID() {
+			return this.playerID;
+		}
+
+		// sets the player's ID.
+		public void setPlayerID(int ID) {
+			this.playerID = ID;
+		}
+
+		// gets the amount of rounds left of a player's jail-time.
+		public int getJailRounds() {
+			return jailRounds;
+		}
+
+		// sets the amount of rounds left of a player's jail-time.
+		public void setJailRounds(int jailRounds) {
+			this.jailRounds = jailRounds;
+		}
+		
+		public int getPrevPosition(){
+			return prevPosition;
+		}
+
+		// gets the player's current position.
+		public int getCurrentPosition() {
+			return currentPosition;
+		}
+
+		// sets the player's current position. ny adding an int to his current position.
+		public void setCurrentPosition(int addToPosition) {
+			prevPosition = currentPosition;
+			currentPosition = addToPosition + currentPosition;
+			if (currentPosition > 40) {
+				currentPosition -= 40; 
+				
+			}
+		}
+		
+		//used to set a player to a specific field.
+		public void changePosition (int newPosition){
+			prevPosition = currentPosition;
+			this.currentPosition = newPosition;
+		}
+
+		//used to set the amount of ferries owned by a player.
+		public void setOwnedFerries(int ferriesOwned) {
+			ownedFerries = ownedFerries + ferriesOwned;
+		}
+		
+		//used to get the amount of ferries owned by a player.
+		public int getOwnedFerries() {
+			return ownedFerries;
+	}
+	public void setOwnedBreweries(int breweriesOwned) {
+		ownedBreweries = ownedBreweries + breweriesOwned;
+	}
+
+	public int getOwnedBreweries() {
+		return ownedBreweries;
+	}
+	
+	public String toString(){
+		return playerName;
+	}
+}
