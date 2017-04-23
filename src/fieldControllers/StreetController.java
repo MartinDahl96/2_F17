28a9960933e02deeca2bbcbf92b8f364c7ad5d34
JanusAA <janus.andreasson@hotmail.com>
2017-04-23 17:41:21 +0200

    edit in streetcontroller

--- src/fieldControllers/StreetController.java
+++ src/fieldControllers/StreetController.java
@@ -10,44 +10,43 @@ import mainControllers.MUI;
 public class StreetController {
 
 	public void sellProperty(Player player, Street s) {
-		
+
 		String input = MUI.getUserString("Indtast feltnummer for at sælge et hus");
 		int fieldNumber = Integer.parseInt(input);
-		
+
 		for (Field field : Board.getFields()) {
-			
-		if(field instanceof Street){
-			
-			if(fieldNumber == field.getFieldID()){
-				if(((Street)field).getOwner() == player){
-					if (field instanceof Street && ((Street)field).getNumOfBuildings()>0){
-						if (((Street)field).getNumOfBuildings() == 5){
-							//Fjerner hotel fra GUI hvis der er et:
-							MUI.setHotel(fieldNumber, false);
-							MUI.SetHouses(fieldNumber, 4);
+
+			if (field instanceof Street) {
+
+				if (fieldNumber == field.getFieldID()) {
+					if (((Street) field).getOwner() == player) {
+						if (field instanceof Street && ((Street) field).getNumOfBuildings() > 0) {
+							if (((Street) field).getNumOfBuildings() == 5) {
+								// Fjerner hotel fra GUI hvis der er et:
+								MUI.setHotel(fieldNumber, false);
+								MUI.SetHouses(fieldNumber, 4);
+							} else {
+								// Fjerner et hus fra gameboard, i GUI'en
+								MUI.SetHouses(fieldNumber, ((Street) field).getNumOfBuildings() - 1);
+							}
+							// Efter hus/hotel grafisk er fjernet, får spilleren
+							// halvdelen af købsprisen tilbage
+							((Street) field).setNumOfBuildings(((Street) field).getNumOfBuildings() - 1);
+							player.setFortune(((Street) field).getBuildPrice() / 2);
 						}
-						else{
-							// Fjerner et hus fra gameboard, i GUI'en
-							MUI.SetHouses(fieldNumber, ((Street)field).getNumOfBuildings()-1);
-						}
-						// Efter hus/hotel grafisk er fjernet, får spilleren halvdelen af købsprisen tilbage
-						((Street)field).setNumOfBuildings(((Street)field).getNumOfBuildings()-1);
-						player.setFortune(((Street)field).getBuildPrice()/2);
 					}
+
 				}
-			
-			
 			}
 		}
-		}
-		
+
 	}
 
 	public void buildProperty(Player player, Street s) {
 
-	//checkIfBuildable(player,s);
+		checkIfBuildable(player, s);
 
-		if (s.buildable(player) == true && player.getFortune() > s.getBuildPrice()) {
+		if (s.isBuildable() == true && player.getFortune() > s.getBuildPrice()) {
 			boolean choice = MUI.getTwoButtons("Vil du bygge et hus eller hvad?", "Ja", "Nej");
 
 			int buildHouse0 = 0;
@@ -82,28 +81,28 @@ public class StreetController {
 
 				if (buildHouse0 == 3) {
 					if (choice == true) {
-						s.setNumOfBuildings(s.getNumOfBuildings()+1);
+						s.setNumOfBuildings(s.getNumOfBuildings() + 1);
 					}
 
 				}
 				if (buildHouse1 == 3) {
 					if (choice == true) {
-						s.setNumOfBuildings(s.getNumOfBuildings()+1);
+						s.setNumOfBuildings(s.getNumOfBuildings() + 1);
 					}
 				}
 				if (buildHouse2 == 3) {
 					if (choice == true) {
-						s.setNumOfBuildings(s.getNumOfBuildings()+1);
+						s.setNumOfBuildings(s.getNumOfBuildings() + 1);
 					}
 				}
 				if (buildHouse3 == 3) {
 					if (choice == true) {
-						s.setNumOfBuildings(s.getNumOfBuildings()+1);
+						s.setNumOfBuildings(s.getNumOfBuildings() + 1);
 					}
 				}
 				if (buildHouse4 == 3) {
 					if (choice == true) {
-						s.setNumOfBuildings(s.getNumOfBuildings()+1);
+						s.setNumOfBuildings(s.getNumOfBuildings() + 1);
 					}
 				}
 
@@ -113,43 +112,34 @@ public class StreetController {
 
 	}
 
-	
-	// HEJ 
-	
-	//TODO: Find ud af om nedenstående kode skal bruges. PT bruges buildable() fra Street i stedet. 
-	
-//	public boolean checkIfBuildable(Player player, Street s){
-//
-//		int count3colors = 0;
-//		int count2colors = 0;
-//
-//		for (Field field : Board.getFields()) {
-//
-//			if(field instanceof Street){
-//			if (((Street) field).getOwner() == player) {
-//
-//				if (((Street) field).getColor().equals("purple") || ((Street) field).getColor().equals("blue")) {
-//					count2colors++;
-//
-//				}
-//
-//				else if (((Street) field).getColor().equals(s.getColor())) {
-//					count3colors++;
-//
-//				}
-//
-//			}
-//			}
-//			if (count3colors == 3 || count2colors == 2) {
-//				s.setBuildablel(true);
-//			}
-//
-//		}
-//		return s.isBuildable();
+	public boolean checkIfBuildable(Player player, Street s) {
 
-	
-		
-		
-	
+		int count3colors = 0;
+		int count2colors = 0;
 
+		for (Field field : Board.getFields()) {
+
+			if (field instanceof Street) {
+				if (((Street) field).getOwner() == player) {
+
+					if (((Street) field).getColor().equals("purple") || ((Street) field).getColor().equals("blue")) {
+						count2colors++;
+
+					}
+
+					else if (((Street) field).getColor().equals(s.getColor())) {
+						count3colors++;
+
+					}
+
+				}
+			}
+			if (count3colors == 3 || count2colors == 2) {
+				s.setBuildable(true);
+			}
+
+		}
+		return s.isBuildable();
+
+	}
 }
--- src/fieldEntities/Street.java
+++ src/fieldEntities/Street.java
@@ -123,75 +123,6 @@ public class Street extends Ownable {
 
 	}
 
-	public void buildProperty(Player player) {
-
-		buildable(player);
-
-		if (buildable == true && player.getFortune() > this.buildPrice) {
-			boolean choice = MUI.getTwoButtons("Vil du bygge et hus eller hvad?", "Ja", "Nej");
-
-			int buildHouse0 = 0;
-			int buildHouse1 = 0;
-			int buildHouse2 = 0;
-			int buildHouse3 = 0;
-			int buildHouse4 = 0;
-
-			for (Field field : Board.getFields()) {
-
-				if (((Street) field).getColor() == this.color) {
-
-					if (this.numOfBuildings == 0) {
-						buildHouse0++;
-
-						if (this.numOfBuildings == 1) {
-							buildHouse1++;
-
-						}
-						if (this.numOfBuildings == 2) {
-							buildHouse2++;
-						}
-						if (this.numOfBuildings == 3) {
-							buildHouse3++;
-						}
-						if (this.numOfBuildings == 4) {
-							buildHouse4++;
-
-						}
-					}
-				}
-
-				if (buildHouse0 == 3) {
-					if (choice == true) {
-						this.numOfBuildings++;
-					}
-
-				}
-				if (buildHouse1 == 3) {
-					if (choice == true) {
-						this.numOfBuildings++;
-					}
-				}
-				if (buildHouse2 == 3) {
-					if (choice == true) {
-						this.numOfBuildings++;
-					}
-				}
-				if (buildHouse3 == 3) {
-					if (choice == true) {
-						this.numOfBuildings++;
-					}
-				}
-				if (buildHouse4 == 3) {
-					if (choice == true) {
-						this.numOfBuildings++;
-					}
-				}
-
-			}
-
-		}
-
-	}
 
 	/**
 	 * Gets and returns the rent for the player to pay the owner of the street
@@ -231,4 +162,12 @@ public class Street extends Ownable {
 		return rent;
 	}
 
+	public void setBuildable(boolean b){
+		buildable = b;
+	}
+	
+	public boolean isBuildable(){
+		return buildable;
+	}
+	
 }
