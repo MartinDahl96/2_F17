d94d476292f402766e685e3b50a8a5d11decad41
fahadali <fahadali@192.168.10.85>
2017-05-01 13:25:57 +0200

    tax edit

--- src/fieldControllers/TaxController.java
+++ src/fieldControllers/TaxController.java
@@ -18,8 +18,6 @@ public class TaxController {
 
 	public void landOnTax(Player player) {
 
-		
-		
 		if (player.getCurrentPosition() == 5) {
 			boolean choice = MUI.getTwoButtons("4000 kr eller 10 % af din beholdning", "4000", "10 % af din beholdning");
 			if (choice == true) {
@@ -36,6 +34,7 @@ public class TaxController {
 
 		else if (player.getCurrentPosition() == 39) {
 			MUI.showMessage("Betal skat: kr. 2000");
+			
 			player.setFortune(-t.getFixedTax());
 		}
 
