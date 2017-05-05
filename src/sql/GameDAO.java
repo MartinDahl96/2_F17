5aa5ff6320e55df7fef6f2868f8e346832763c4e
FAS16 <fahadalisajad@hotmail.com>
2017-05-05 16:56:09 +0200

    Betaversion 2

--- src/sql/GameDAO.java
+++ src/sql/GameDAO.java
@@ -1,54 +1,61 @@
 package sql;
 
 import java.sql.SQLException;
-
-import entities.ChanceDeck;
+import java.sql.Statement;
 
 public class GameDAO {
-
-	//attributes
+	
+	private Connector c = new Connector();
 	private PlayerDAOimp pDAO = new PlayerDAOimp();
 	private FieldDAOimp fDAO = new FieldDAOimp();
 	private ChanceDAOimp cDAO = new ChanceDAOimp();
 
-	/**
-	 * used to load the game.
-	 * @param playerID is the ID of the player to load.
-	 * @throws SQLException
-	 */
-	public void loadGame(int playerID) throws SQLException {
-		try ())
+
+	
+	public void loadGame(){
+		try {
+			pDAO.getPlayers();
+			pDAO.getOwnedBreweries();
+			pDAO.getOwnedFerries();
+			fDAO.getOwnable();
+			fDAO.getBuildingsOnStreet();
+			cDAO.getChanceCards();
 			
 		} catch (SQLException e) {
-			e.printStackTrace();
-		}	}
+			
+		}
+	}
 
-	/**
-	 * used to save the current game.
-	 * @throws Exception
-	 */
 	public void saveGame() {
 
 		try {
 			pDAO.insertPlayer();
 			fDAO.insertOwnable();
-			cDAO.insertUpdateCards();
+			cDAO.insertCards();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	/**
-	 * used to update the current saved game to a new state of the game.
-	 * @throws Exception
-	 */
 	public void updateSave() {
 		try {
 			pDAO.updatePlayer();
 			fDAO.updateOwnable();
-			cDAO.insertUpdateCards();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void newGame(){
+		String resetTables = "call resetTables();";
+		try {
+			Statement stmt = c.getConnection().createStatement();
+			stmt.executeUpdate(resetTables);
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("New game");
 	}
 }
